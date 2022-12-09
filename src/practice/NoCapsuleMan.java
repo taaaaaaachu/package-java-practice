4c262f1d53d6d5eb8f375691a723f1f9d3099a81
Tachu <stitch_ditto_0j@icloud.com>
2022-12-09 15:49:02 +0900

    カプセル化

---
+++ src/practice/NoCapsuleMan.java
@@ -0,0 +1,23 @@
+package practice;
+
+public class NoCapsuleMan {
+    private String name;
+    private int age;
+
+    public NoCapsuleMan(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
