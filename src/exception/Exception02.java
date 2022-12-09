005abf23f0dddbf8d287dd14f814362ff28c78f9
Tachu <stitch_ditto_0j@icloud.com>
2022-12-09 15:11:40 +0900

    commit

---
+++ src/exception/Exception02.java
@@ -0,0 +1,19 @@
+package exception;
+
+/**
+ * Exception02
+ */
+public class Exception02 {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("100 ÷ 0 は？");
+            int result = 100 / 0;
+            System.out.println("計算結果" + result);
+        } catch (Exception e) {
+            System.out.println("例外が発生");
+        } finally {
+            System.out.println("プログラム終了");
+        }
+    }
+}
